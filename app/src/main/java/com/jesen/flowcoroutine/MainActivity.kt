a793398a2fbc3aa60333bd36a05a0d7f96cc1c6a
Jesen0823 <36436771+Jesen0823@users.noreply.github.com>
2021-11-10 16:44:06 +0800

    [FlowCoroutine-download] 01. 冷流flow下载demo

--- app/src/main/java/com/jesen/flowcoroutine/MainActivity.kt
+++ app/src/main/java/com/jesen/flowcoroutine/MainActivity.kt
@@ -2,10 +2,16 @@ package com.jesen.flowcoroutine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jesen.flowcoroutine.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val mBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(mBinding.root)
     }
 }
