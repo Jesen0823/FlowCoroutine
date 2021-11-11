1ad2419941ab6b44f9a3420019dbd2f412e546f9
Jesen0823 <36436771+Jesen0823@users.noreply.github.com>
2021-11-11 18:18:56 +0800

    [FlowCoroutine-paging3] 13. 修复旋转屏幕数据丢失问题

--- jetpackpaging3/src/main/java/com/jesen/jetpackpaging/vm/ExamViewModel.kt
+++ jetpackpaging3/src/main/java/com/jesen/jetpackpaging/vm/ExamViewModel.kt
@@ -1,23 +1,31 @@
 package com.jesen.jetpackpaging.vm
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.jesen.jetpackpaging.model.Question
 import com.jesen.jetpackpaging.paging.ExamPagingSource
 import kotlinx.coroutines.flow.Flow
 
 class ExamViewModel : ViewModel() {
 
-    fun loadExam(): Flow<PagingData<Question>> {
-        return Pager(
+    // 为了把请求结果保存在ViewModel,不发生数据丢失，以属性存储下来
+    private val result by lazy {
+        Pager(
             config = PagingConfig(
                 pageSize = 5,
                 initialLoadSize = 10, // 第一次加载数量，如果不设置的话是 pageSize * 3
                 prefetchDistance = 2,
             ),
             pagingSourceFactory = { ExamPagingSource() }
-        ).flow
+        // 缓存数据到ViewModel
+        ).flow.cachedIn(viewModelScope)
+    }
+
+    fun loadExam(): Flow<PagingData<Question>> {
+        return result
     }
 }
